db0fc1243d0c52cc7b6fc7eedb19f7712421b449
DESKTOP-KBUILC4\david <david042691@gmail.com>
2018-12-09 14:38:38 -0800

    Update Complier Final Project/Complier Final Project/testingcpp.cpp

--- Complier Final Project/Complier Final Project/testingcpp.cpp
+++ Complier Final Project/Complier Final Project/testingcpp.cpp
@@ -8,126 +8,103 @@ void Nospace(ifstream& input, ofstream& output);
 
 int main()
 {
+
 	ifstream currentopenfile;
-	ofstream writefilehere("finalp2.txt");
-	currentopenfile.open("finalp1.txt");
+	ofstream writefilehere("finalp3.cpp");
+	currentopenfile.open("finalp2.txt");
 	if (currentopenfile.fail())
 	{
 		cout << "Input file could not be opened or found. \n";
 		return 0;
 	}
 
-	Nospace(currentopenfile, writefilehere);
-	currentopenfile.close();
-	writefilehere.close();
-	cout << "done" << endl;
-	system("pause");
-	return 0;
-}
-
-
-void Nospace(ifstream& input, ofstream& output)
-{
-	string CurrentWord;
-	int i = 0;
-	char Currentline[100];
-	int k = 0;
-	int z = 0;
-	int marktheendofline = 0;
-	bool nonewlineneeded = false;
-	bool starcomment = false;
-	while (input.good())
+	string Currentline;
+	bool Variablenext = false;
+	while (currentopenfile.good())
 	{
-		i = 0;
-		// get each string from input
-		getline(input, CurrentWord, ' ');
-		//loop through current string until it ends
-		while (CurrentWord[i] != '\0')
+		int i = 0;
+		getline(currentopenfile, Currentline, '\n');
+
+		if (!Currentline.find("program"))
 		{
-
-			Currentline[k] = CurrentWord[i];
-			i++;
-			k++;
-			if (Currentline[0] == '\n')	// if you begin a new line with another \n then don't add it to the output
-			{
-				k = 0;
-				Currentline[0] = '\0';
-			}
-			else if (Currentline[k - 1] == '\t')// get rid of tab
-			{
-				k -= 1;
-			}
-			else if (Currentline[0] == 'e' && Currentline[1]== 'n' && Currentline[2]== 'd')
-			{
-				output << "end";
-				break;
-			}
-			else if (CurrentWord[i] == '\n')	// if run into \n process the current line first before moving on to the next line
-			{
-
-				while (z != k)
-				{
-
-					if (Currentline[z] == '/' && Currentline[z + 1] == '*')
-					{
-						starcomment = true;
-						break;
-					}
-					else if (Currentline[z] == ';' && Currentline[z + 1] == '/' && Currentline[z + 2] == '/')
-					{
-						//nonewlineneeded = true;
-						output << Currentline[z];
-						break;
-					}
-					else if (Currentline[z] == '/' && Currentline[z + 1] == '/')
-					{
-						nonewlineneeded = true;
-						break;
-					}
-					else
-					{
-						output << Currentline[z];
-						z++;
-					}
-				}
-
-				if (nonewlineneeded == false && starcomment == false)
-				{
-					output << '\n';
-					z = 0;
-					k = 0;
-					for (int a = 0; a < 100; a++)
-						Currentline[a] = '\0';
-				}
-
-				else if (nonewlineneeded == true)
-				{
-					z = 0;
-					k = 0;
-					nonewlineneeded = false;
-					for (int a = 0; a < 100; a++)
-						Currentline[a] = '\0';
-				}
-
-				if (starcomment == true)
-				{
-					for (int a = 0; a < 100; a++)
-						Currentline[a] = NULL;
-					k = 0;
-					z = 0;
-					starcomment = false;
-				}
-			}
+			writefilehere << "#include";
+			writefilehere << '"';
+			writefilehere << "stdafx.h";
+			writefilehere << '"';
+			writefilehere << "\n";
+			writefilehere << "#include <iostream>\n";
+			writefilehere << "using namespace std;\n";
+			continue;
 		}
 
-		if (Currentline[k - 1] == ')' || Currentline[k - 1] == '(' || Currentline[k - 1] == ';' || Currentline[k - 1] == '/')
+		if (!Currentline.find("var"))
 		{
-			k = k;
+			Variablenext = true;
+			continue;
+		}
+
+		if (Currentline[i] == 's' &&Currentline[i + 1] == 'h' &&Currentline[i + 2] == 'o' &&Currentline[i + 3] == 'w')
+		{
+			writefilehere << "cout <<";
+			i += 4;
+			for (i; i < Currentline.length(); i++)
+			{
+				writefilehere << Currentline[i];
+			}
+			writefilehere << "\n";
+			continue;
+		}
+		if (!Currentline.find("end"))
+		{
+			writefilehere << "}\n";
+			continue;
+		}
+
+		else if (!Currentline.find("begin"))
+		{
+			writefilehere << "int main()\n{\n";
+			continue;
+		}
+
+		if (Variablenext == true) {
+			writefilehere << "int ";
+			while (Variablenext == true)
+			{
+				/*if (Currentline[i] == 'i' && Currentline[i + 1] == 'n' && Currentline[i + 2] == 't')			// Use this code here to make cpp file similar to what he have on the prompt
+				{
+					writefilehere << "int";
+					i += 7;
+					continue;
+				}
+				if (Currentline[i] == ';')
+				{
+					writefilehere << ";\n";
+					Variablenext = false;
+					break;
+				}
+				writefilehere << Currentline[i];
+				i++;*/
+
+				writefilehere << Currentline[i];
+				i++;
+				if (Currentline[i] == ':')
+				{
+					writefilehere << ";\n";
+					Variablenext = false;
+				}
+
+			}
 		}
 		else
 		{
-			Currentline[k] = ' ';
-			k++;
+			writefilehere << Currentline;
+			writefilehere << "\n";
 		}
 	}
+	currentopenfile.close();
+	writefilehere.close();
+	system("gcc -o finalp3.cpp");
+	cout << "done" << endl;
+	system("pause");
+	return 0;
 }
